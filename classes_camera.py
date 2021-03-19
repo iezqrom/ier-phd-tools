15c790a6a8e325cfbf766faf599d296394814138
Ivan Ezquerra Romano <ivanezqrom@eduroam-int-dhcp-97-228-158.ucl.ac.uk>
2021-03-19 11:25:10 +0000

    getting touch out of the way

--- classes_camera.py
+++ classes_camera.py
@@ -939,7 +939,7 @@ class TherCam(object):
 
                 if globals.stimulus == 2:
                     dif = mean_diff_buffer - dataC
-                    dif[dataC <= 26] = 0
+                    dif[dataC <= 28] = 0
                     maxdif = np.max(dif)
                     indxdf, indydf = np.where(dif == maxdif)
                     print(indxdf[0], indydf[0])

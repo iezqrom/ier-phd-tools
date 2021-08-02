02ce92bda1b93d7ce38a1df750b557134fec49f4
Ivan Ezquerra-Romano <ivan.eromano@gmail.com>
2021-08-02 20:21:47 +0100

    staircase class done for derma experiment

--- staircases.py
+++ staircases.py
@@ -1,5 +1,6 @@
 import time
 import pickle
+import matplotlib.pyplot as plt
 
 class Staircase():
     """
@@ -19,6 +20,15 @@ class Staircase():
         self.total_reversals = total_reversals
         self.name = name
 
+        self.reversal_values = []
+        self.estimated_point = None
+
+        self.list_stimulations = []
+        self.list_tracked_stimulations = []
+
+        self.list_to_plot = {0: {'trial': [], 'stimulation': []}, 1: {'trial': [], 'stimulation': []}}
+
+
     def __str__(self):
         return f'Name: {self.name}; Reversals: {self.reversals}; Tracker: {self.tracker}; Stimulation: {self.stimulation}; Tracked stimulation: {self.tracked_stimulation}'
 
@@ -36,6 +46,13 @@ class Staircase():
         '''
         self.response = response
         self.reversed_bool = False
+
+        self.list_stimulations.append(self.stimulation)
+        self.list_tracked_stimulations.append(self.tracked_stimulation)
+
+        self.list_to_plot[self.response]['trial'].append(self.trial)
+        self.list_to_plot[self.response]['stimulation'].append(self.stimulation)
+
         if self.last_response != response and self.last_response is not None:
             self.tracker = 0
             self.reversals += 1
@@ -43,15 +60,13 @@ class Staircase():
             self.first_ramp = False
             if not self.first_ramp and self.reversals == 1:
                 print('\nTracking algorithm triggered\n')
+                self.reversal_values.append(self.stimulation)
 
     def XupYdownFixedStepSizesTrackingAlgorithm(self, move_down, move_up, step_down, step_up):
         '''
             Execute tracking algorithm for a stairse X UP / Y DOWN with fixed step sizes (but the length of the steps up and down can be of different size)
         '''
         self.tracker += 1
-        print(self.response)
-
-        time.sleep(1)
 
         if not self.first_ramp:
             if self.tracker == move_down and self.response == 1:
@@ -77,3 +92,33 @@ class Staircase():
             self.stimulation = lower_boundary
         else:
             self.stimulation = self.tracked_stimulation
+
+    def estimateValue(self, drop_reversals = 3):
+        '''
+            Estimate the value based on the self.reversal_values list
+        '''
+        self.estimated_point = sum(self.reversal_values[drop_reversals:])/len(self.reversal_values[drop_reversals:])
+
+    def plotStaircase(self, path_figs, name, ylabel, ylim, fig = None, ax = None):
+        '''
+            Plot staircase
+        '''
+
+        if not fig and not ax:
+            fig, ax = plt.subplots(1)
+
+        ax.plot(list(range(1, len(self.list_stimulations) + 1)), self.list_stimulations, color = 'k')
+        ax.plot(list(range(1, len(self.list_tracked_stimulations) + 1)), self.list_tracked_stimulations, color = 'g')
+
+        ax.scatter(self.list_to_plot[0]['trial'], self.list_to_plot[0]['stimulation'], color = 'red')
+        ax.scatter(self.list_to_plot[1]['trial'], self.list_to_plot[1]['stimulation'], color = 'k')
+
+        ax.axhline(self.estimated_point, color = 'k')
+
+        ax.set_ylim(ylim)
+        ax.set_ylabel(ylabel)
+        ax.set_xlabel('Trials')
+
+        plt.savefig(f'{path_figs}/{name}.png')
+
+        plt.show()

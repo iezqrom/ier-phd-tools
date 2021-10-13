0970227698c6b17f5356527fba6ae1248eb89f4d
Ivan Ezquerra-Romano <ivan.eromano@gmail.com>
2021-10-13 15:42:09 +0100

    reduce grid and more documentation

--- classes_conds.py
+++ classes_conds.py
@@ -15,17 +15,24 @@ class ConditionsHandler():
 ########################## FUNCTIONS ############################
 #################################################################
 
+
 def sdt_setup(n_trials, conds, randomised=True):
     """
         Function to set-up trials for a Signal Detection Theory experiment.
         The number of trials will be distributed equally across the number of conditions.
-        For instance, if we have 1 condition (e.g. cold stimulation with touch), 
+        For instance, if we have 1 condition (e.g. cold stimulation with touch),
         and you set n_trials = 10, there'll be 10 trials of this condition.
         However, if we have 2 conditions (e.g. cold stimulation with and without touch),
         and you set n_trials = 10, there'll be 5 trials of each condition.
         Stimulus absent and present are coded with 0s (absent) and 1s (present), respectively.
         Conditions are coded from 0-n.
         Condition and stimulus absent/present come in tuples.
+
+        position 0: condition
+        position 1: catch trial
+        position 2: touch
+        position 3: reverse
+        position 4: fake
     """
 
     stimulations = []
@@ -41,16 +48,16 @@ def sdt_setup(n_trials, conds, randomised=True):
         n_conds = np.repeat(code_conds, n_cond_trials, axis = 0)
         unique, counts = np.unique(n_conds, return_counts=True)
         print(counts)
-        
+
         for u, c in zip(unique, counts):
             abs_pres = np.repeat([0, 1], c, axis = 0)
-            
+
             for ap in abs_pres:
                 stimulations.append((u, ap))
         if randomised:
             np.random.shuffle(stimulations)
         stimulations = stimulations[:n_trials]
-        
+
     else:
         code_conds = np.arange(conds)
         n_cond_trials = n_trials/conds
@@ -58,10 +65,10 @@ def sdt_setup(n_trials, conds, randomised=True):
         n_conds = np.repeat(code_conds, n_cond_trials, axis = 0)
         unique, counts = np.unique(n_conds, return_counts=True)
         # print(counts)
-        
+
         for u, c in zip(unique, counts):
             abs_pres = np.repeat([0, 1], c/2, axis = 0)
-            
+
             for ap in abs_pres:
                 stimulations.append((u, ap))
         if randomised:
